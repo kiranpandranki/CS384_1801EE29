326bbf431fffbb85c2398e35dd6d2e77986ac150
kiranpandranki <pandrankikiran2017@gmail.com>
2020-09-17 00:06:08 +0530

    Modified printGP function, this time without using '**' .

--- Assignment1/tutorial01.py
+++ Assignment1/tutorial01.py
@@ -42,8 +42,10 @@ def power(num1, num2):  # num1 ^ num2
 
 def printGP(a, r, n):
     gp = []
+    multiplier = 1
     for i in range(n):
-        gp.append(a * r**i)
+        gp.append(a * multiplier)
+        multiplier *= r
     return gp
 
 # Python 3 program to print AP.  arithmetic Progression
@@ -61,3 +63,6 @@ def printAP(a, d, n):
 def printHP(a, d, n):
     hp = []
     return hp
+
+
+print(printGP(2, 3, 5))
