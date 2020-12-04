9b5fd5b40db8a05ef133671482ca8ed29f8a4b0f
RiKan47 <56057035+RiKan47@users.noreply.github.com>
2020-12-04 17:03:38 +0530

    p1, created a root window using tkinter

---
+++ Projects/P1 Quiz_via_CSV/p1_main.py
@@ -0,0 +1,18 @@
+from tkinter import *
+from tkinter.messagebox import showinfo
+import sqlite3
+import csv
+import os
+import datetime
+import hashlib
+import pandas as pd
+import time as t
+cwd = os.getcwd()
+
+def main_menu():
+  pass
+
+root = Tk()
+root.geometry("1100x300")
+main_menu()
+root.mainloop()
