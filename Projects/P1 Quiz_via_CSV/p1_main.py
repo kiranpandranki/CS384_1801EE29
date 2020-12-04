ce3be1ef1c1bff59c21695d1c82d14e0fc6573cb
kiranpandranki <59500632+kiranpandranki@users.noreply.github.com>
2020-12-04 17:09:54 +0530

    p1, added main_menu fuction into P1Quiz_via_Csv file

--- Projects/P1 Quiz_via_CSV/p1_main.py
+++ Projects/P1 Quiz_via_CSV/p1_main.py
@@ -10,6 +10,17 @@ import time as t
 cwd = os.getcwd()
 
 def main_menu():
+  main_menu_frame = Frame(root, bg='grey')
+  Button(main_menu_frame, text='Register', width=20, relief=RIDGE,
+         command=lambda: [register(), main_menu_frame.destroy()]).pack()
+  Button(main_menu_frame, text='Login', width=20, relief=RIDGE,
+         command=lambda: [login(), main_menu_frame.destroy()]).pack()
+  Button(main_menu_frame, text='Exit', width=20,
+         relief=RIDGE, command=quit).pack()
+  main_menu_frame.place(relx=0.5, rely=0.5, anchor=CENTER)
+def register():
+  pass
+def login():
   pass
 
 root = Tk()
